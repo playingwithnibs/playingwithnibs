adebcf3f83690f4dcbaa7aa5ccf7fda4fb75cd2c
Adriano Sorcioni <adri.sorcioni@gmail.com>
2019-05-31 15:28:28 +0200

    base project structure

---
+++ PlayWithNibs/Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// Quits the player when the user hits escape
+
+public class MenuController : MonoBehaviour
+{
+    public void quitButtonPressed() => Application.Quit();
+
+
+    public void onPlayButton()
+    {
+        SceneManager.LoadScene("GameSelection", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Menu");     
+    }                                                
+    public void onBackButton()                       
+    {                                                
+        SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("GameSelection");
+    }
+}
