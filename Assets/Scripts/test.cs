48395521f0277d0cb0ef0a7138c86d313e8f25ca
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-04 22:58:42 +0200

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,7 +5,10 @@ namespace Application
 {
   public class test {
     public static void Main() {
-      Console.WriteLine("cisco");
+      // Console.WriteLine(new SimulationSolution().isTms(
+      //   new Tdcs(UnitMeasure.MILLIAMPERE, 130, Pulse.HIGH,
+      //   new BrainZone(BrainZoneNames.CP5, Position.UPPER, 
+      //   ElectrodeType.NEUTRAL), TdcsStimulator.DEFAULT)));
     }
   }
 }
