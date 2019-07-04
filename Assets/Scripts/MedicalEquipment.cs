533e65934889c6186c087223918fdf91f14550e8
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-04 22:58:25 +0200

    Update MedicalEquipment.cs

--- Assets/Scripts/MedicalEquipment.cs
+++ Assets/Scripts/MedicalEquipment.cs
@@ -1,5 +1,6 @@
 using System;
 using Application;
+using System.Collections.Generic;
 
 namespace Application
 {
@@ -7,14 +8,14 @@ namespace Application
     public UnitMeasure unitMeasure; 
     public double intensity;
     public Pulse pulse; 
-    public BrainZone brainZone;
+    public HashSet<BrainZone> brainZones;
 
     public MedicalEquipment(UnitMeasure unitMeasure, double intensity, 
-      Pulse pulse, BrainZone brainZone) {
+      Pulse pulse, HashSet<BrainZone> brainZones) {
       this.unitMeasure = unitMeasure;
       this.intensity = intensity;
       this.pulse = pulse;
-      this.brainZone = brainZone;
+      this.brainZones = brainZones;
     }
   }
 }
