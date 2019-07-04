1a3f41d431ee9e054ec58a27ad0e582d579169b2
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-04 11:45:49 +0200

    Create SimulationSolution class

---
+++ Assets/Scripts/SimulationSolution.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application {
+  public class SimulationSolution {
+    public string solution = "";
+
+    private Dictionary<string, string> idealSolutions 
+        = new Dictionary<string, string>();
+
+    public SimulationSolution() {
+      idealSolutions.add("DEPRESSION_TMS8_90_%_PULSEHIGH_NOSTIMULATION_DLPFC", 
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_91_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_92_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_93_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_94_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_95_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_96_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_97_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_98_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_99_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_100_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_101_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_102_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_103_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_104_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_105_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_106_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_107_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_108_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+      idealSolutions.add("DEPRESSION_TMS8_109_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_110_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_111_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_112_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_113_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_114_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_115_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_116_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_117_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_118_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+        
+      idealSolutions.add("DEPRESSION_TMS8_119_%_PULSEHIGH_NOSTIMULATION_DLPFC",
+        "GOOD");
+
+    }
+  }
+}
