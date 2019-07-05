c6e350803601d13066f464b8feb0ab42eac16103
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 12:21:08 +0200

    Create StimulationType.cs

---
+++ Assets/Scripts/StimulationType.cs
@@ -0,0 +1,11 @@
+using System;
+using Application;
+
+namespace Application {
+  public enum StimulationType { 
+    NO = 0,
+    ANODAL = 1, 
+    CATHODAL = 2,
+    MAGNETIC = 3 // we probably won't need this, but, you know...
+  }
+} 
