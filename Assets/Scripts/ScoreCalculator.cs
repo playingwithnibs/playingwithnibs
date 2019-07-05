bf38f4c321f9f6ab3a6f87ba94a63173cffa4dfe
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 13:48:28 +0200

    Update ScoreCalculator.cs

--- Assets/Scripts/ScoreCalculator.cs
+++ Assets/Scripts/ScoreCalculator.cs
@@ -13,6 +13,8 @@ namespace Application {
 
     public ScoreCalculator() {}
 
+    // to get the start and end timestamps:
+    // https://stackoverflow.com/questions/21219797/how-to-get-correct-timestamp-in-c-sharp/21219819
     public double computeScore(Outcome simulationOutcome, 
       DateTime simulationStartTime, DateTime simulationEndTime) {
       
