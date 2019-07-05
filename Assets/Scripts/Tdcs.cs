93124bef05c25d36116908a05cc574eb55ce478d
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 13:06:05 +0200

    Update Tdcs.cs

--- Assets/Scripts/Tdcs.cs
+++ Assets/Scripts/Tdcs.cs
@@ -6,9 +6,10 @@ namespace Application
 {
   public class Tdcs : MedicalEquipment {
     public TdcsStimulator stimulator;
-    public Tdcs(UnitMeasure unitMeasure, double intensity,
-      Pulse pulse, HashSet<BrainZone> brainZones, TdcsStimulator stimulator) 
-        : base(unitMeasure, intensity, pulse, brainZones) {
+    public Tdcs(UnitMeasure unitMeasure, double intensity, Pulse pulse, 
+      BrainZonesArray brainZones, TdcsStimulator stimulator,
+      StimulationType stimulationType) 
+        : base(unitMeasure, intensity, pulse, brainZones, stimulationType) {
       this.stimulator = stimulator;
     }
   }
