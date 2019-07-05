863a723c87b4763930accec6b43eac1f62a06761
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 13:06:01 +0200

    Update MedicalEquipment.cs

--- Assets/Scripts/MedicalEquipment.cs
+++ Assets/Scripts/MedicalEquipment.cs
@@ -8,18 +8,18 @@ namespace Application
     public UnitMeasure unitMeasure; 
     public double intensity;
     public Pulse pulse; 
-    public BrainZonesArray[] brainZones;
+    public BrainZonesArray brainZones;
 
     public StimulationType stimulationType;
 
     public MedicalEquipment(UnitMeasure unitMeasure, double intensity, 
-      Pulse pulse, BrainZonesArray[] brainZones, 
+      Pulse pulse, BrainZonesArray brainZones,
       StimulationType stimulationType) {
         this.unitMeasure = unitMeasure;
         this.intensity = intensity;
         this.pulse = pulse;
-        this.brainZones = brainZones;
         this.stimulationType = stimulationType;
+        this.brainZones = brainZones;
     }
   }
 }
