3e7f2a5fb44cf47b8522d4edd0a56bed496d32f9
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 13:43:44 +0200

    Create ScoreCalculator.cs

---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Application;
+
+namespace Application {
+  public class ScoreCalculator {
+
+    private const int BONUS_TIME = 20;
+
+    private const int MALUS_MOLTIPLICATOR = 5;
+
+    public ScoreCalculator() {}
+
+    public double computeScore(Outcome simulationOutcome, 
+      DateTime simulationStartTime, DateTime simulationEndTime) {
+      
+      return (int)simulationOutcome + BONUS_TIME - 
+        (simulationEndTime - simulationStartTime) * 5;
+    }
+  }
+}
