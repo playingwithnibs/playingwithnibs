bdb7abaf4657f693a22b4f333280cca6c368f76e
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-05 14:28:16 +0200

    Update ScoreCalculator.cs

--- Assets/Scripts/ScoreCalculator.cs
+++ Assets/Scripts/ScoreCalculator.cs
@@ -7,19 +7,27 @@ using Application;
 namespace Application {
   public class ScoreCalculator {
 
-    private const int BONUS_TIME = 20;
+    private const int TIME_BONUS = 20;
 
-    private const int MALUS_MOLTIPLICATOR = 5;
+    private const int SAVED_TIME_BONUS_MOLTIPLICATOR = 5;
+
+    private const int DEVICE_CONFIG_SCORE_PERCENTAGE = 40;
 
     public ScoreCalculator() {}
 
+    public double computeMedicalEquipmentScore(Outcome simulationOutcome) {
+      return (int)simulationOutcome * DEVICE_CONFIG_SCORE_PERCENTAGE;
+    }
+
+    public double computeOutcomeScore(Outcome simulationOutcome) {
+      return (int)simulationOutcome;
+    }
+
     // to get the start and end timestamps:
     // https://stackoverflow.com/questions/21219797/how-to-get-correct-timestamp-in-c-sharp/21219819
-    public double computeScore(Outcome simulationOutcome, 
-      DateTime simulationStartTime, DateTime simulationEndTime) {
-      
-      return (int)simulationOutcome + BONUS_TIME - 
-        (simulationEndTime - simulationStartTime) * 5;
-    }
+    public double computeTimeBonus(DateTime simStart, DateTime simEnd) {
+        return TIME_BONUS +
+          (simEnd - simStart).TotalSeconds * SAVED_TIME_BONUS_MOLTIPLICATOR;
+      }
   }
 }
