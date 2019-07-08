31b3ab7c41b7cd558b064bb98bed6afa891f6afb
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-08 19:25:47 +0200

    Update PlayerManager.cs
    Add score computing logic interface to PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Application;
+using System;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
     public Application.Pathology pathology;
+    public double startTime; 
+
+    public double endTime;
+
+    public Outcome outcome;
+
+    public double getCurrentTimestampInSeconds() 
+    {
+        return (DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1))
+            .TotalSeconds;
+    } 
+
+    public double computeScore()
+    {
+        ScoreCalculator sc = new ScoreCalculator();
+
+        return sc.computeTimeBonus(startTime, endTime) + 
+            sc.computeOutcomeScore(outcome) + 
+            sc.computeMedicalEquipmentScore(outcome);
+    }
+
 
     void Awake()
     {
