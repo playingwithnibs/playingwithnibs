6d3a0d96a7751a1557f5e0951160c8a7ea384388
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-08 20:20:07 +0200

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,64 +5,68 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 using static Constants;
 
-public class GameController : MonoBehaviour
-{
-    private string macchinario;
-    private string voltaggio;
-    public GameObject macchina1;
-    public GameObject macchina2;
-    public GameObject parametro1;
-    public GameObject parametro2;
-    public GameObject sliderA;
-    public GameObject sliderP;
-
-    private void Start()
+namespace Application {
+    public class GameController : MonoBehaviour
     {
-       
-    }
+        //private string macchinario;
+        //private string voltaggio;
+        public GameObject macchina1;
+        public GameObject macchina2;
+        public GameObject parametro1;
+        public GameObject parametro2;
+        public GameObject sliderA;
+        public GameObject sliderP;
 
-    public void onTdcsSelected()
-    {
-        macchinario = TDCS;
-        iniziaFaseDue();
-    }
-    public void onTmsSelected()
-    {
-        macchinario = TMS;
-        iniziaFaseDue();
-    }
+        private void Start()
+        {
+        
+        }
 
-    public void iniziaFaseDue()
-    {
-        SceneManager.LoadScene(GAME_2, LoadSceneMode.Single);
-        macchina1.SetActive(false);
-        macchina2.SetActive(false);
-        parametro1.SetActive(true);
-        parametro2.SetActive(true);
-    }
+        public void onTdcsSelected()
+        {
+            PlayerManager.Instance.medicalEquipment = new Tdcs();
+            iniziaFaseDue();
+        }
+        public void onTmsSelected()
+        {
+            PlayerManager.Instance.medicalEquipment = new Tms();
+            iniziaFaseDue();
+        }
 
-    public void scegliVoltaggioA()
-    {
-        voltaggio = "Ahm";
-        sliderA.SetActive(true);
-        sliderP.SetActive(false);     
-    }
+        public void iniziaFaseDue()
+        {
+            SceneManager.LoadScene(GAME_2, LoadSceneMode.Single);
+            macchina1.SetActive(false);
+            macchina2.SetActive(false);
+            parametro1.SetActive(true);
+            parametro2.SetActive(true);
+        }
 
-    public void scegliVoltaggioP()
-    {
-        voltaggio = "%";
-        sliderA.SetActive(false);
-        sliderP.SetActive(true);
-    }
+        public void scegliVoltaggioA()
+        {
+            PlayerManager.Instance.medicalEquipment.unitMeasure = 
+                UnitMeasure.MILLIAMPERE;
+            sliderA.SetActive(true);
+            sliderP.SetActive(false);     
+        }
 
-    public void exitSession()
-    {
-        SceneManager.LoadScene(Constants.GAME_SELECTION, LoadSceneMode.Single);
-    }
+        public void scegliVoltaggioP()
+        {
+            PlayerManager.Instance.medicalEquipment.unitMeasure =
+                UnitMeasure.PERCENTAGE_OF_MT;
+            sliderA.SetActive(false);
+            sliderP.SetActive(true);
+        }
 
-    private void Awake()
-    {
-        Debug.Log(PlayerManager.Instance.pathology.name);
-    }
+        public void exitSession()
+        {
+            SceneManager.LoadScene(GAME_SELECTION, LoadSceneMode.Single);
+        }
 
+        private void Awake()
+        {
+            Debug.Log(PlayerManager.Instance.pathology.name);
+        }
+
+    }
 }
