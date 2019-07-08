91319ee9b9545112a8991a885cbb70a143fed8d3
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-08 17:50:29 +0200

    Update Pathology.cs

--- Assets/Scripts/Pathology.cs
+++ Assets/Scripts/Pathology.cs
@@ -10,7 +10,25 @@ namespace Application {
 
     public Pathology(PathologyName name, Position position) {
       this.name = name;
-      this.position = position;
+
+      if (name == PathologyName.POST_STROKE_HAND || 
+        name == PathologyName.POST_STROKE_APHASIA)
+        this.position = position;
+      else 
+        position = Position.NO;
+    }
+
+    public Pathology() {
+      Array values = Enum.GetValues(typeof(PathologyName));
+      this.name = (PathologyName)values
+        .GetValue(new Random().Next(values.Length));
+
+      values = Enum.GetValues(typeof(Position));
+      if (name == PathologyName.POST_STROKE_HAND || 
+        name == PathologyName.POST_STROKE_APHASIA)
+        this.position = (Position)values.GetValue(new Random().Next(1, 3));
+      else 
+        position = Position.NO;
     }
   }
 } 
