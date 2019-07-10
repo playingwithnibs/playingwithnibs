8022fc55c8e11fb97fd0c0b1489cf4bcfad26de4
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 17:43:17 +0200

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,8 +7,8 @@ namespace Application
     public static void Main() {
       SimulationSolution ss = new SimulationSolution();
 
-      Pathology p = new Pathology(PathologyName.POST_STROKE_HAND, 
-        Position.RIGHT);
+      // Pathology p = new Pathology(PathologyName.POST_STROKE_HAND, 
+      //   Position.RIGHT);
 
       // Tms tms = new Tms(UnitMeasure.PERCENTAGE_OF_MT, 9, Pulse.HIGH, 
       //   new BrainZonesArray(), TmsStimulator.EIGHT, StimulationType.NO);
@@ -22,7 +22,7 @@ namespace Application
       tdcs.brainZones.brainZones[(int)BrainZoneNames.M1].position = 
         Position.LEFT;
       
-      Console.WriteLine(ss.getOutcomePostStrokeHand(tdcs, p));
+      Console.WriteLine(ss.getOutcomePostStrokeHand(tdcs, new MedicalReport()));
     }
   }
 }
