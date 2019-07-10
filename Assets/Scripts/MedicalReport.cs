e392053e729920f2fe9db99c955db73c74f18a65
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 17:42:52 +0200

    Create MedicalReport.cs

---
+++ Assets/Scripts/MedicalReport.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Application {
+
+  public class MedicalReport {
+    public PersonName name;
+
+    public PersonSurname surname;
+
+    public DateTime dateOfBirth;
+
+    public Gender gender;
+
+    public Pathology pathology;
+
+    public MedicalReport() {
+      gender = new Random().Next(0, 41) >= 20 ? (Gender) 40 : 0;
+
+      name = (PersonName)Enum
+        .GetValues(typeof(PersonName))
+        .GetValue(new Random().Next(0, 3)) + (int)gender;
+
+      surname = (PersonSurname)Enum
+        .GetValues(typeof(PersonSurname))
+        .GetValue(new Random().Next(0, 7));
+
+      DateTime start = new DateTime(1970, 1, 1);
+      dateOfBirth = start
+        .AddDays(new Random().Next((DateTime.Today - start).Days));
+
+      pathology = new Pathology();
+    }
+  }
+}
