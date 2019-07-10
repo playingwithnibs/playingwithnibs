11235e5624835c59cab2835ba3fe27d06234dc10
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 17:43:09 +0200

    Update BrainZoneNames.cs

--- Assets/Scripts/BrainZoneNames.cs
+++ Assets/Scripts/BrainZoneNames.cs
@@ -3,11 +3,11 @@ using Application;
 
 namespace Application {
   public enum BrainZoneNames { 
-  DLPFC = 0, 
-  M1 = 1, 
-  SO = 2, 
-  O = 3, 
-  CP5 = 4, 
-  CP6 = 5
+    DLPFC = 0, 
+    M1 = 1, 
+    SO = 2, 
+    O = 3, 
+    CP5 = 4, 
+    CP6 = 5
   }
 } 
