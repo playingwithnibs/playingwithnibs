d441b82d84da27ea3e0a89dc64d16b8fb460fbe3
fortym2 <30431538+fortym2@users.noreply.github.com>
2019-07-10 16:26:18 +0200

    Medical equip. recap in" Gioco-2" changes by user selection. - Added Resources/Sprites folder for sprites

--- Assets/Scripts/MachineConfigurationController.cs
+++ Assets/Scripts/MachineConfigurationController.cs
@@ -7,6 +7,7 @@ public class MachineConfigurationController : MonoBehaviour
 
 {
     private Text subtitle;
+    private SpriteRenderer medicalEquipmentRecap;
     private PlayerManager pm;
 
     // Start is called before the first frame update
@@ -14,8 +15,13 @@ public class MachineConfigurationController : MonoBehaviour
     {
         pm = PlayerManager.getInstance();
         subtitle = GameObject.Find("Canvas/Subtitle").GetComponent<Text>();
+        medicalEquipmentRecap = GameObject.Find("Canvas/medical-eq-recap").GetComponent<SpriteRenderer>();
 
         subtitle.text = "Configure the " + pm.medicalEquipment.name + " you have selected";
+
+        Debug.Log(medicalEquipmentRecap.sprite.name);
+        medicalEquipmentRecap.sprite = Resources.Load("Sprites/medical-eq-recap-tdcs", typeof(Sprite)) as Sprite;
+        Debug.Log(medicalEquipmentRecap.sprite.name);
     }
 
     // Update is called once per frame
