ddf9d59e490a16bdf1bfabd736c7a98c4b37686c
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 18:18:15 +0200

    Update BrainZonesArray.cs

--- Assets/Scripts/BrainZonesArray.cs
+++ Assets/Scripts/BrainZonesArray.cs
@@ -1,7 +1,3 @@
-using System;
-using Application;
-using System.Collections.Generic;
-
 namespace Application
 {
   public class BrainZonesArray {
@@ -11,20 +7,21 @@ namespace Application
 
     public BrainZonesArray() {
       brainZones = new BrainZone[6];
-      this.brainZones[0] = 
+      
+      brainZones[0] = 
         new BrainZone(BrainZoneNames.DLPFC, Position.UPPER, ElectrodeType.NO);
-      this.brainZones[1] = 
+      brainZones[1] = 
         new BrainZone(BrainZoneNames.M1, Position.UPPER, ElectrodeType.NO);
-      this.brainZones[2] = 
+      brainZones[2] = 
         new BrainZone(BrainZoneNames.SO, Position.UPPER, ElectrodeType.NO);
-      this.brainZones[3] = 
+      brainZones[3] = 
         new BrainZone(BrainZoneNames.O, Position.UPPER, ElectrodeType.NO);
-      this.brainZones[4] = 
+      brainZones[4] = 
         new BrainZone(BrainZoneNames.CP5, Position.LEFT, ElectrodeType.NO);
-      this.brainZones[5] = 
+      brainZones[5] = 
         new BrainZone(BrainZoneNames.CP6, Position.RIGHT, ElectrodeType.NO);
 
-      this.countActiveZones = 0;
+      countActiveZones = 0;
     }
 
     // tested
