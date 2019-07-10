30a0ba6d58fb72241f04e9d9a035cb1602ca70d4
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 21:40:05 +0200

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -12,17 +12,26 @@ namespace Application
 
       // Tms tms = new Tms(UnitMeasure.PERCENTAGE_OF_MT, 9, Pulse.HIGH, 
       //   new BrainZonesArray(), TmsStimulator.EIGHT, StimulationType.NO);
-      
-      Tdcs tdcs = new Tdcs(UnitMeasure.MILLIAMPERE, 0.2568742684, Pulse.NO,
-        new BrainZonesArray(), TdcsStimulator.DEFAULT, StimulationType.NO);
-      
-      tdcs.brainZones.activateZone(BrainZoneNames.DLPFC, ElectrodeType.NEGATIVE);
-      tdcs.brainZones.activateZone(BrainZoneNames.SO, ElectrodeType.POSITIVE);
-      
-      tdcs.brainZones.brainZones[(int)BrainZoneNames.M1].position = 
-        Position.LEFT;
-      
-      Console.WriteLine(ss.getOutcomePostStrokeHand(tdcs, new MedicalReport()));
+
+      // Tdcs tdcs = new Tdcs(UnitMeasure.MILLIAMPERE, 0.2568742684, Pulse.NO,
+      //   new BrainZonesArray(), TdcsStimulator.DEFAULT, StimulationType.NO);
+
+      // tdcs.brainZones.activateZone(BrainZoneNames.DLPFC, ElectrodeType.NEGATIVE);
+      // tdcs.brainZones.activateZone(BrainZoneNames.SO, ElectrodeType.POSITIVE);
+
+      // tdcs.brainZones.brainZones[(int)BrainZoneNames.M1].position = 
+      //   Position.LEFT;
+
+      // Console.WriteLine(ss.getOutcomePostStrokeHand(tdcs, new MedicalReport()));
+
+      MedicalReport mr = new MedicalReport();
+      Console.WriteLine(mr.animojiPath);
+      Console.WriteLine(mr.dateOfBirth);
+      Console.WriteLine(mr.gender);
+      Console.WriteLine(mr.name);
+      Console.WriteLine(mr.surname);
+      Console.WriteLine(mr.pathology.name);
+      Console.WriteLine(mr.pathology.position);
     }
   }
 }
