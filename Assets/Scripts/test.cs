7422a149560165f72be4db18a30393abca56b9f0
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 15:11:11 +0200

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,10 +5,24 @@ namespace Application
 {
   public class test {
     public static void Main() {
-      // Console.WriteLine(new SimulationSolution().isTms(
-      //   new Tdcs(UnitMeasure.MILLIAMPERE, 130, Pulse.HIGH,
-      //   new BrainZone(BrainZoneNames.CP5, Position.UPPER, 
-      //   ElectrodeType.NEUTRAL), TdcsStimulator.DEFAULT)));
+      SimulationSolution ss = new SimulationSolution();
+
+      Pathology p = new Pathology(PathologyName.POST_STROKE_HAND, 
+        Position.RIGHT);
+
+      // Tms tms = new Tms(UnitMeasure.PERCENTAGE_OF_MT, 9, Pulse.HIGH, 
+      //   new BrainZonesArray(), TmsStimulator.EIGHT, StimulationType.NO);
+      
+      Tdcs tdcs = new Tdcs(UnitMeasure.MILLIAMPERE, 0.2568742684, Pulse.NO,
+        new BrainZonesArray(), TdcsStimulator.DEFAULT, StimulationType.NO);
+      
+      tdcs.brainZones.activateZone(BrainZoneNames.DLPFC, ElectrodeType.NEGATIVE);
+      tdcs.brainZones.activateZone(BrainZoneNames.SO, ElectrodeType.POSITIVE);
+      
+      tdcs.brainZones.brainZones[(int)BrainZoneNames.M1].position = 
+        Position.LEFT;
+      
+      Console.WriteLine(ss.getOutcomePostStrokeHand(tdcs, p));
     }
   }
 }
