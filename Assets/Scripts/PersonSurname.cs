859081eba6bd2c12b844384189ed28908a991598
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 17:43:00 +0200

    Create PersonSurname.cs

---
+++ Assets/Scripts/PersonSurname.cs
@@ -0,0 +1,14 @@
+using System;
+using Application;
+
+namespace Application {
+  public enum PersonSurname { 
+    Rossi = 0,
+    Bianchi = 1,
+    Neri = 2,
+    Verdi = 3,
+    Gialli = 4,
+    Nebbio = 5,
+    Rosaty = 6
+  }
+} 
