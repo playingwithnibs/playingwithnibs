d47fb521d855d3fd100d15e5289a6a1c668bbd26
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-10 17:42:56 +0200

    Create PersonName.cs

---
+++ Assets/Scripts/PersonName.cs
@@ -0,0 +1,13 @@
+using System;
+using Application;
+
+namespace Application {
+  public enum PersonName { 
+    Simone = 0,
+    Alessandro = 1,
+    Lorenzo = 2,
+    Simona = 40,
+    Alessandra = 41,
+    Lorenza = 42
+  }
+} 
