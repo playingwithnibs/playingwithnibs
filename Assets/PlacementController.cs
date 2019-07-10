c3351311af81fb51a57e9f2f9266e152c277b852
fortym2 <30431538+fortym2@users.noreply.github.com>
2019-07-10 21:02:46 +0200

    Created PlacementController to handle Gioco-3

---
+++ Assets/PlacementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Application;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlacementController : MonoBehaviour
+{
+    private PlayerManager pm;
+
+    private Button
+        backButton,
+        forwardButton;
+    private SpriteRenderer
+        medicalEquipmentRecap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = PlayerManager.getInstance();
+
+        backButton = GameObject.Find("back-button").GetComponent<Button>();
+        forwardButton = GameObject.Find("forward-button").GetComponent<Button>();
+        medicalEquipmentRecap = GameObject.Find("medical-eq-recap").GetComponent<SpriteRenderer>();
+
+        medicalEquipmentRecap.sprite = Resources.Load("Sprites/medical-eq-recap-" + pm.medicalEquipment.ToString().ToLower(), typeof(Sprite)) as Sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
