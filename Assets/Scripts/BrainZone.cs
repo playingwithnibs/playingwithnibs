8f18da65b32c215ac0873e83dc190c74c948bebf
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-11 10:43:36 +0200

    Update BrainZone.cs

--- Assets/Scripts/BrainZone.cs
+++ Assets/Scripts/BrainZone.cs
@@ -1,34 +1,31 @@
-﻿using System;
-
-namespace Application
+﻿namespace Application
 {
   public class BrainZone {
     public BrainZoneNames brainZoneName;
+
     public Position position;
+
     public ElectrodeType electrodeType;
+
     public BrainZone(BrainZoneNames brainZoneName, Position position, 
       ElectrodeType electrodeType) {
-      this.brainZoneName = brainZoneName;
-      this.position = position;
-      this.electrodeType = electrodeType;
+        this.brainZoneName = brainZoneName;
+        this.position = position;
+        this.electrodeType = electrodeType;
     }
 
-    public override int GetHashCode() { 
-      return (int)brainZoneName;
-    }
+    public override int GetHashCode() { return (int)brainZoneName; }
     
-    public override bool Equals(object obj) { 
+    public override bool Equals(object obj) {
       if (!(obj is BrainZone) || obj == null) 
         return false;
       
       BrainZone bz = (BrainZone)obj;
 
-      return bz.brainZoneName.Equals(this.brainZoneName);
+      return bz.brainZoneName.Equals(brainZoneName);
     }
 
-    public bool isActive() {
-      return electrodeType != ElectrodeType.NO;
-    }
+    public bool isActive() { return electrodeType != ElectrodeType.NO; }
   }
 
 }
