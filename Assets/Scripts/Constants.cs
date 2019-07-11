643c2005eba7d16b6f3c34389a46d45691d640fc
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-11 10:43:41 +0200

    Update Constants.cs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,14 +1,19 @@
-﻿public static class Constants
-{
-    // SCENES
-    public const string MENU = "Menu";
-    public const string GAME_SELECTION = "GameSelection";
-    public const string PATHOLOGIES = "Patologie";
-    public const string GAME_1 = "Gioco-1";
-    public const string GAME_2 = "Gioco-2";
-    public const string GAME_3 = "Gioco-3";
+﻿public static class Constants {
+  // SCENES
+  public const string MENU = "Menu";
 
-    // MACHINES
-    public const string TDCS = "tDCS";
-    public const string TMS = "TMS";
+  public const string GAME_SELECTION = "GameSelection";
+
+  public const string PATHOLOGIES = "Patologie";
+
+  public const string GAME_1 = "Gioco-1";
+
+  public const string GAME_2 = "Gioco-2";
+
+  public const string GAME_3 = "Gioco-3";
+
+  // MACHINES
+  public const string TDCS = "tDCS";
+
+  public const string TMS = "TMS";
 }
