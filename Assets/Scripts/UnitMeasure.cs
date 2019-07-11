c5ce5fd7d7bd550abb38f161c78421524051a187
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-11 10:44:00 +0200

    Update UnitMeasure.cs

--- Assets/Scripts/UnitMeasure.cs
+++ Assets/Scripts/UnitMeasure.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-namespace Application {
+﻿namespace Application {
   public enum UnitMeasure {
     NO = 0,
-    PERCENTAGE_OF_MT = 1, 
+
+    PERCENTAGE_OF_MT = 1,
+
     MILLIAMPERE = 2
   }
 }
