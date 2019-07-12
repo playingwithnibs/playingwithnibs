49921bda7a70aa87ec2ca9ea413f347f0fec636d
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-12 15:55:37 +0200

    Update Pathology.cs
    Add pathologies descriptions

--- Assets/Scripts/Pathology.cs
+++ Assets/Scripts/Pathology.cs
@@ -10,66 +10,75 @@ namespace Application {
     public string[] descriptions;
 
     public Pathology(PathologyName name) {
+      Array values = Enum.GetValues(typeof(Position));
+      if (name == PathologyName.POST_STROKE_HAND ||
+        name == PathologyName.POST_STROKE_APHASIA)
+        position = (Position)values.GetValue(new Random().Next(1, 3));
+      else
+        position = Position.NO;
+
       descriptions = new string[6];
 
-      descriptions[0] = "DEPRESSION DESCRIPTION";
-      descriptions[1] = "POST_STROKE_HAND DESCRIPTION";
+      descriptions[0] = "I'm always sad beacuse I only see negative things " +
+        "happening in my life.\nI can't seem to control this negative " + 
+        "feelings anymore... 😭";
+      descriptions[1] = "I recently had a stroke and now my " +
+        position.ToString().ToLower() + " hand is constantly shaking... 😐";
       descriptions[2] = "POST_STROKE_APHASIA DESCRIPTION";
       descriptions[3] = "OTHER PATHOLOGY DESC";
       descriptions[4] = "LIKE ABOVE";
       descriptions[5] = "LIKE ABOVE";
 
       this.name = name;
-
-      Array values = Enum.GetValues(typeof(Position));
-      if (name == PathologyName.POST_STROKE_HAND || 
-        name == PathologyName.POST_STROKE_APHASIA)
-        position = (Position)values.GetValue(new Random().Next(1, 3));
-      else 
-        position = Position.NO;
     }
 
     public Pathology(PathologyName name, Position p)
     {
-      descriptions = new string[6];
-
-      descriptions[0] = "DEPRESSION DESCRIPTION";
-      descriptions[1] = "POST_STROKE_HAND DESCRIPTION";
-      descriptions[2] = "POST_STROKE_APHASIA DESCRIPTION";
-      descriptions[3] = "OTHER PATHOLOGY DESC";
-      descriptions[4] = "LIKE ABOVE";
-      descriptions[5] = "LIKE ABOVE";
-
-      this.name = name;
-
       if (name == PathologyName.POST_STROKE_HAND ||
         name == PathologyName.POST_STROKE_APHASIA)
         position = p;
       else
         position = Position.NO;
-    }
-
-    public Pathology() {
+      
       descriptions = new string[6];
 
-      descriptions[0] = "DEPRESSION DESCRIPTION";
-      descriptions[1] = "POST_STROKE_HAND DESCRIPTION";
+      descriptions[0] = "I'm always sad beacuse I only see negative things " +
+        "happening in my life.\nI can't seem to control this negative " +
+        "feelings anymore... 😭";
+      descriptions[1] = "I recently had a stroke and now my " +
+        position.ToString().ToLower() + " hand is constantly shaking... 😐";
       descriptions[2] = "POST_STROKE_APHASIA DESCRIPTION";
       descriptions[3] = "OTHER PATHOLOGY DESC";
       descriptions[4] = "LIKE ABOVE";
       descriptions[5] = "LIKE ABOVE";
-      
+
+      this.name = name;
+    }
+
+    public Pathology() {
       Array values = Enum.GetValues(typeof(PathologyName));
-      
+
       name = (PathologyName)values
         .GetValue(new Random().Next(0, 2));
 
       values = Enum.GetValues(typeof(Position));
-      if (name == PathologyName.POST_STROKE_HAND || 
+      if (name == PathologyName.POST_STROKE_HAND ||
         name == PathologyName.POST_STROKE_APHASIA)
         position = (Position)values.GetValue(new Random().Next(1, 3));
-      else 
+      else
         position = Position.NO;
+        
+      descriptions = new string[6];
+
+      descriptions[0] = "I'm always sad beacuse I only see negative things " +
+        "happening in my life.\nI can't seem to control this negative " +
+        "feelings anymore... 😭";
+      descriptions[1] = "I recently had a stroke and now my " +
+        position.ToString().ToLower() + " hand is constantly shaking... 😐";
+      descriptions[2] = "POST_STROKE_APHASIA DESCRIPTION";
+      descriptions[3] = "OTHER PATHOLOGY DESC";
+      descriptions[4] = "LIKE ABOVE";
+      descriptions[5] = "LIKE ABOVE";
     }
 
     public string getDescription() {
