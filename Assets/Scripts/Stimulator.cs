19bb025257db8a2fc16883d3b44ad3b80d0b45ef
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-12 09:56:58 +0200

    Update Stimulator.cs

--- Assets/Scripts/Stimulator.cs
+++ Assets/Scripts/Stimulator.cs
@@ -1,5 +1,18 @@
 ﻿namespace Application
 {
   public class Stimulator {
+    public ElectrodeType electrodeType;
+
+    public int tapCounter;
+
+    public Stimulator() {
+      electrodeType = ElectrodeType.NO;
+      tapCounter = 0;
+    }
+
+    public Stimulator(ElectrodeType electrodeType) {
+      this.electrodeType = electrodeType;
+      tapCounter = (int)electrodeType;
+    }
   } 
 }
