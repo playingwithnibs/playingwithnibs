f380e399ee85b7b5c71865c3dbd535fe67f4f071
fortym2 <30431538+fortym2@users.noreply.github.com>
2019-07-13 22:26:26 +0200

    Medical report partial injection in first game screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 using static Constants;
 
 namespace Application {
     public class GameController : MonoBehaviour
     {
 
+        private MedicalReport mr;
+
+        private Text
+            name,
+            sex,
+            age,
+            description,
+            pathology;
+
         private void Start()
         {
             PlayerManager.getInstance().medicalReport = new MedicalReport();
+            mr = PlayerManager.getInstance().medicalReport;
             Debug.Log(PlayerManager.getInstance().medicalReport.pathology.name + " " + PlayerManager.getInstance().medicalReport.pathology.position);
             PlayerManager.getInstance().startTime = PlayerManager.getInstance().getCurrentTimestampInSeconds();
+
+            initMedicalReportUI();
+            
         }
 
         public void onTdcsSelected()
@@ -37,6 +51,20 @@ namespace Application {
         {
             // TODO: void the simulation session
             SceneManager.LoadScene(GAME_SELECTION, LoadSceneMode.Single);
+        }
+
+        private void initMedicalReportUI()
+        {
+            name = GameObject.Find("Medical report text/name").GetComponent<Text>();
+            sex = GameObject.Find("Medical report text/sex").GetComponent<Text>();
+            age = GameObject.Find("Medical report text/age").GetComponent<Text>();
+            description = GameObject.Find("Medical report text/description").GetComponent<Text>();
+            pathology = GameObject.Find("Medical report text/pathology").GetComponent<Text>();
+
+            name.text = mr.name + " " + mr.surname;
+            sex.text = "Sex: " + mr.gender;
+            age.text = "Birth: " + mr.dateOfBirth.ToShortDateString();
+            pathology.text = "Pathology: " + mr.pathology;
         }
     }
 }
