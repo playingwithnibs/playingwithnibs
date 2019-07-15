09b4b0f30e9d3943469a0a1fdb1e8380dbff07f0
Daniele Solombrino <daniele.solombrino@gmail.com>
2019-07-15 15:18:46 +0200

    Update PlacementController.cs
    Clean code

--- Assets/PlacementController.cs
+++ Assets/PlacementController.cs
@@ -9,8 +9,6 @@ using UnityEngine.SceneManagement;
 
 public class PlacementController : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;
-
     public GameObject cam;
     public VideoClip videoClip;
     private PlayerManager pm;
@@ -71,37 +69,6 @@ public class PlacementController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Will attach a VideoPlayer to the main camera.
-        cam = GameObject.Find("Main Camera");
-
-        // VideoPlayer automatically targets the camera backplane when it is added
-        // to a camera object, no need to change videoPlayer.targetCamera.
-        videoPlayer = cam.AddComponent<VideoPlayer>();
-
-        // Play on awake defaults to true. Set it to false to avoid the url set
-        // below to auto-start playback since we're in Start().
-        videoPlayer.playOnAwake = false;
-
-        // By default, VideoPlayers added to a camera will use the far plane.
-        // Let's target the near plane instead.
-        //videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
-
-        // This will cause our Scene to be visible through the video being played.
-        videoPlayer.targetCameraAlpha = 0.5F;
-
-        // Set the video to play. URL supports local absolute or relative paths.
-        // Here, using absolute.
-        videoPlayer.url = "/Video/explosionTrim2.mp4";
-
-        // Skip the first 100 frames.
-        //videoPlayer.frame = 100;
-
-        // Restart from beginning when done.
-        videoPlayer.isLooping = false;
-
-        // Each time we reach the end, we slow down the playback by a factor of 10.
-        //videoPlayer.loopPointReached += EndReached;
-
         pm = PlayerManager.getInstance();
 
         pm.time -= Time.deltaTime;
@@ -471,17 +438,14 @@ public class PlacementController : MonoBehaviour
                 pm.medicalReport,
                 new BrainZonesArray(brainZones));
 
-        if (pm.outcome == Outcome.EXPLOSION) {
-            // Start playback. This means the VideoPlayer may have to prepare (reserve
-            // resources, pre-load a few frames, etc.). To better control the delays
-            // associated with this preparation one can use videoPlayer.Prepare() along with
-            // its prepareCompleted event.
-            videoPlayer.Play();
-        }
-
         // Debug.Log(pm.medicalReport + "\n" + pm.medicalEquipment +
         //     "\n" + pm.brainZones);
         //Debug.Log(pm.outcome);
+
+        if (pm.medicalEquipment.name.Equals("TMS"))
+            audioSourceMagn.Play();
+        else
+            audioSourceElect.Play();
 
         SceneManager.LoadScene(Constants.RESULT, LoadSceneMode.Single);
     }
